c762010ef9586be49bebb85b782a97b9f2323dbd
bunsan <oeunbunsan07@gmail.com>
2026-05-22 13:33:15 +0700

    Add default port

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 // Entry file
 import { connectDB } from "./src/config/db.js";
 import http from "http";
-import { app } from "./app.js"
-
+import { app } from "./app.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const startServer = async () => {
-  await connectDB();
-  const server = http.createServer(app);
+  try {
+    await connectDB();
 
-  server.listen(process.env.PORT, "0.0.0.0", () => {
-    console.log("The server is running on PORT : " + process.env.PORT);
-  })
+    const PORT = process.env.PORT || 5000;
+
+    const server = http.createServer(app);
+
+    server.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error while starting the Server:", error);
+    process.exit(1);
+  }
 };
 
-startServer().catch((error) => {
-  console.log("Error while starting the Server : ", error);
-  process.exit(1);
-})
+startServer();
